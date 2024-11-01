45745cf0c77358e2fabab2be80d61c0a949c6473
Houstin <houstin00@gmail.com>
2024-11-01 23:01:05 +0300

    moved toggle darkmode button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -122,51 +122,6 @@ function Navbar() {
         </a>
 
         <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          {/* Dark mode toggle button */}
-          <button
-            id="theme-toggle"
-            type="button"
-            onClick={toggleDarkMode}
-            className="text-xl text-gray-500 dark:text-gray-400 lg:hover:bg-gray-200 lg:dark:hover:bg-gray-700 rounded-lg p-2 mx-4"
-          >
-            <svg
-              className="w-6 h-6 text-gray-800 dark:text-white"
-              id="theme-toggle-dark-icon"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fillRule="evenodd"
-                d="M11.675 2.015a.998.998 0 0 0-.403.011C6.09 2.4 2 6.722 2 12c0 5.523 4.477 10 10 10 4.356 0 8.058-2.784 9.43-6.667a1 1 0 0 0-1.02-1.33c-.08.006-.105.005-.127.005h-.001l-.028-.002A5.227 5.227 0 0 0 20 14a8 8 0 0 1-8-8c0-.952.121-1.752.404-2.558a.996.996 0 0 0 .096-.428V3a1 1 0 0 0-.825-.985Z"
-                clipRule="evenodd"
-              />
-            </svg>
-
-           
-            <svg
-              className="w-6 h-6 text-gray-800 dark:text-white"
-              id="theme-toggle-light-icon"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M12 5V3m0 18v-2M7.05 7.05 5.636 5.636m12.728 12.728L16.95 16.95M5 12H3m18 0h-2M7.05 16.95l-1.414 1.414M18.364 5.636 16.95 7.05M16 12a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z"
-              />
-            </svg>
-          </button>
-
           {/* User menu */}
           {currentUser ? (
             <div>
@@ -186,24 +141,69 @@ function Navbar() {
               {/* Dropdown menu */}
               {userDropdownVisible && (
                 <div className="z-50 m-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600 absolute right-0">
-                 
-
-
-             
                   <ul className="py-2" aria-labelledby="user-menu-button">
-
                     <li>
-                    <Link
+                      <Link
                         to="/profile"
                         className="block px-4 py-2 text-sm text-gray-700 rounded-lg hover:bg-blue-200 dark:hover:bg-blue-600 dark:text-gray-200 dark:hover:text-white"
                       >
-                                            <span className="block text-sm text-gray-900 dark:text-white">
-                      {currentUser.username}
-                    </span>
-                    <span className="block text-sm text-gray-500 truncate dark:text-gray-400">
-                      {currentUser.email}
-                    </span>
+                        <span className="block text-sm text-gray-900 dark:text-white">
+                          {currentUser.username}
+                        </span>
+                        <span className="block text-sm text-gray-500 truncate dark:text-gray-400">
+                          {currentUser.email}
+                        </span>
                       </Link>
+                    </li>
+
+                    <li>
+                      {/* Dark mode toggle button */}
+                      <button
+                        onClick={toggleDarkMode}
+                        className="block w-full text-left px-4 py-2 text-sm text-gray-700 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                      >
+                        {/* Moon icon for dark mode */}
+                        <svg
+                          className={`w-6 h-6 mr-2 text-gray-800 dark:text-white ${
+                            isDarkMode ? "hidden" : "inline-block"
+                          }`}
+                          id="theme-toggle-dark-icon"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          fill="currentColor"
+                          viewBox="0 0 24 24"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M11.675 2.015a.998.998 0 0 0-.403.011C6.09 2.4 2 6.722 2 12c0 5.523 4.477 10 10 10 4.356 0 8.058-2.784 9.43-6.667a1 1 0 0 0-1.02-1.33c-.08.006-.105.005-.127.005h-.001l-.028-.002A5.227 5.227 0 0 0 20 14a8 8 0 0 1-8-8c0-.952.121-1.752.404-2.558a.996.996 0 0 0 .096-.428V3a1 1 0 0 0-.825-.985Z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                        {/* Sun icon for light mode */}
+                        <svg
+                          className={`w-6 h-6 mr-2 text-gray-800 dark:text-white ${
+                            isDarkMode ? "inline-block" : "hidden"
+                          }`}
+                          id="theme-toggle-light-icon"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                        >
+                          <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M12 5V3m0 18v-2M7.05 7.05 5.636 5.636m12.728 12.728L16.95 16.95M5 12H3m18 0h-2M7.05 16.95l-1.414 1.414M18.364 5.636 16.95 7.05M16 12a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z"
+                          />
+                        </svg>
+                        {isDarkMode ? "Light Mode" : "Dark Mode"}
+                      </button>
                     </li>
 
                     <li>
